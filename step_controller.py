574f8de641a5eaa08b5b0ebcf6555e7b1366b880
onur.tascioglu <onur.tascioglu@ardictech.com>
2024-03-24 01:06:14 +0300

    added step motor controller

---
+++ step_controller.py
@@ -0,0 +1,111 @@
+import time
+import board
+from adafruit_motor import stepper
+from adafruit_motorkit import MotorKit
+from constants import *
+
+
+class StepController():
+    """
+    Class to control the stepper motors.
+
+    Attributes:
+        kit (MotorKit): Adafruit MotorKit object
+
+    Note:
+        Stepper1 = horizontal movements
+        Stepper2 = vertical movements
+
+        ## Motor informations
+        -200 steps per revolution
+        -15cm movement for 5m distance (1 step)
+        -12cm movement for 4m distance (1 step)
+        -
+    """
+    current_direction_horizontal = IDLE
+    current_direction_vertical = IDLE
+
+    def __init__(self) -> None:
+        self.kit = MotorKit(i2c=board.I2C())
+
+    def move_left(self, step):
+        print("Moving left")
+        self.kit.stepper1.onestep(direction=stepper.FORWARD, style=step)
+        time.sleep(0.01)
+    
+    def move_right(self, step):
+        print("Moving right")
+        self.kit.stepper1.onestep(direction=stepper.BACKWARD, style=step)
+        time.sleep(0.01)
+    
+    def move_up(self, step):
+        print("Moving up")
+        self.kit.stepper2.onestep(direction=stepper.FORWARD, style=step)
+        time.sleep(0.01)
+    
+    def move_down(self, step):
+        print("Moving down")
+        self.kit.stepper2.onestep(direction=stepper.BACKWARD, style=step)
+        time.sleep(0.01)
+
+    def release_horizontal(self):
+        print("Releasing horizontal")
+        self.kit.stepper1.release()
+    
+    def release_vertical(self):
+        print("Releasing vertical")
+        self.kit.stepper2.release()
+
+    def release(self):
+        self.kit.stepper1.release()
+        self.kit.stepper2.release()
+    
+    def set_directions(self, direction_x, direction_y):
+        self.current_direction_horizontal = direction_x
+        self.current_direction_vertical = direction_y
+    
+    def move(self):
+        while True:
+            if self.current_direction_horizontal == LEFT:
+                self.move_left(step=stepper.SINGLE)
+            elif self.current_direction_horizontal == RIGHT:
+                self.move_right(step=stepper.SINGLE)
+            elif self.current_direction_horizontal == IDLE:
+                self.release_horizontal()
+        
+            if  self.current_direction_vertical == UP:
+                self.move_up(step=stepper.SINGLE)
+            elif  self.current_direction_vertical == DOWN:
+                self.move_down(step=stepper.SINGLE)
+            elif  self.current_direction_vertical == IDLE:
+                self.release_vertical()
+            time.sleep(0.01)
+
+    @staticmethod
+    def get_stepper_next_directions(mid_x, mid_y):
+        """
+        Get the stepper motor direction based on the object's midpoint.
+        
+        Args:
+            mid_x (int): x-coordinate of the object's midpoint
+            mid_y (int): y-coordinate of the object's midpoint
+        
+        Returns:
+            tuple: horizontal and vertical directions
+        """
+        horizontal = IDLE
+        vertical = IDLE
+
+        ## check if the object is within the target lock threshold
+        if abs(mid_x - CENTER_X) > TARGET_LOCK_THRESHOLD_PIXELS:
+            if mid_x < CENTER_X:
+                horizontal = LEFT
+            elif mid_x > CENTER_X:
+                horizontal = RIGHT
+
+        if abs(mid_y - CENTER_Y) > TARGET_LOCK_THRESHOLD_PIXELS:
+            if mid_y < CENTER_Y:
+                vertical = UP
+            elif mid_y > CENTER_Y:
+                vertical = DOWN
+        return horizontal, vertical
--- yolo.py
+++ yolo.py
@@ -1,11 +1,12 @@
 import cv2
-import math
 import time
+import threading
 from ultralytics import YOLO
+from constants import *
+from step_controller import StepController
 
-classNames = [0]
+
 model = YOLO("yolo-Weights/yolov8n.pt")
-
 
 def capture_and_set(width, height):
     cap = cv2.VideoCapture(0)
@@ -13,29 +14,43 @@ def capture_and_set(width, height):
     cap.set(4, height)
     return cap
 
-cap = capture_and_set(640,480)
+def get_object_midpoints(x1, y1, x2, y2):
+    return (x1 + x2) // 2, (y1 + y2) // 2
+
+cap = capture_and_set(IMAGE_WIDTH, IMAGE_HEIGHT)
+
+step_controller = StepController()
+motor_thread = threading.Thread(target=step_controller.move)
+motor_thread.start()
+
 while True:
     try:
         start = time.time()
         success, img = cap.read()
         if img is None:
-            cap = capture_and_set(640,480)
-        results = model.predict(img, stream=True, save=False, classes=classNames,imgsz = (256, 256))
+            cap = capture_and_set(IMAGE_WIDTH, IMAGE_HEIGHT)
+        results = model.predict(img, stream=True, save=False, classes=CLASS_NAMES,imgsz = (YOLO_IMAGE_WIDTH, YOLO_IMAGE_HEIGHT), verbose=False)
         cv2.imshow('Webcam', img)
 
         # coordinates
         for r in results:
             boxes = r.boxes
 
+            if len(boxes) != 1:
+                continue
+
             for box in boxes:
                 # bounding box
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-                # put box in cam
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                ## yolo origin points (0,0) is top-letf of the image
+                mid_x, mid_y = get_object_midpoints(x1, y1, x2, y2)
+                direction_x, direction_y = StepController.get_stepper_next_directions(mid_x, mid_y)
+                step_controller.set_directions(direction_x, direction_y)
 
-                confidence = math.ceil((box.conf[0]*100))/100
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), BB_COLOR, 3)
 
                 cls = int(box.cls[0])
 
@@ -47,14 +62,13 @@ while True:
                 thickness = 2
 
                 try:
-                    cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                    cv2.putText(img, CLASS_NAMES[cls], org, font, fontScale, color, thickness)
                 except:
                     pass
 
         end = time.time()
         fps = 1/(end-start)
-        fps = int(fps)
-        fps = str(fps)
+        fps = str(int(fps))
         cv2.putText(img, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
 
         cv2.imshow('Webcam', img)
