1431079e4ea91dd0c16e7c3bcdc4b81ffa2f70f1
onur.tascioglu <onur.tascioglu@ardictech.com>
2024-04-01 17:35:23 +0300

    updated test

--- test.py
+++ test.py
@@ -4,31 +4,27 @@ import time
 from constants import *
 from adafruit_motor import stepper
 from step_controller import StepController
+from adafruit_motorkit import MotorKit
+import board
 
-
-step_controller = StepController()
-# motor_thread = threading.Thread(target=step_controller.move)
-# motor_thread.start()
+kit = MotorKit(i2c=board.I2C())
 
 if sys.argv[1] == "--left":
     for i in range(10):
-        step_controller.move_left(stepper.DOUBLE)
+        kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
         time.sleep(0.1)
 
 if sys.argv[1] == "--right":
     for i in range(10):
-        step_controller.move_right(stepper.DOUBLE)
+        kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
         time.sleep(0.1)
 
 if sys.argv[1] == "--up":
     for i in range(10):
-        step_controller.move_up(stepper.DOUBLE)
+        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
         time.sleep(0.1)
 
 if sys.argv[1] == "--down":
     for i in range(10):
-        step_controller.move_down(stepper.DOUBLE)
+        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
         time.sleep(0.1)
-
-step_controller.set_run_flag(False)
-step_controller.release()
