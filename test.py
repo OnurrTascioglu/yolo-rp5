6f0b361f3a82d5215a659e588a502ac8157e8780
onur.tascioglu <onur.tascioglu@ardictech.com>
2024-04-03 18:32:21 +0300

    updated test

--- test.py
+++ test.py
@@ -11,22 +11,26 @@ kit = MotorKit(i2c=board.I2C())
 kit.frequency = 1600
 
 if sys.argv[1] == "--left":
-    for i in range(10000):
+    for i in range(2000):
+        print(i)
         kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
         time.sleep(0.005)
 
 if sys.argv[1] == "--right":
-    for i in range(10000):
+    for i in range(2000):
+        print(i)
         kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
         time.sleep(0.005)
 
 if sys.argv[1] == "--up":
-    for i in range(10000):
+    for i in range(2000):
+        print(i)
         kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
         time.sleep(0.005)
 
 if sys.argv[1] == "--down":
-    for i in range(10000):
+    for i in range(2000):
+        print(i)
         kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
         time.sleep(0.005)
 
