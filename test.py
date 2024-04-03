3fb854707409db3a16542275af1c79a59300d58d
onur.tascioglu <onur.tascioglu@ardictech.com>
2024-04-03 18:26:04 +0300

    updated test

--- test.py
+++ test.py
@@ -8,27 +8,27 @@ from adafruit_motorkit import MotorKit
 import board
 
 kit = MotorKit(i2c=board.I2C())
-kit.frequency = 3200
+kit.frequency = 1600
 
 if sys.argv[1] == "--left":
-    for i in range(2000):
+    for i in range(10000):
         kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
-        time.sleep(0.01)
+        time.sleep(0.005)
 
 if sys.argv[1] == "--right":
-    for i in range(2000):
+    for i in range(10000):
         kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
-        time.sleep(0.01)
+        time.sleep(0.005)
 
 if sys.argv[1] == "--up":
-    for i in range(2000):
+    for i in range(10000):
         kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
-        time.sleep(0.01)
+        time.sleep(0.005)
 
 if sys.argv[1] == "--down":
-    for i in range(2000):
+    for i in range(10000):
         kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
-        time.sleep(0.01)
+        time.sleep(0.005)
 
 kit.stepper1.release()
 kit.stepper2.release()
