fb8ca7c2b16549862dedd7c089a25daacf981b8a
onur.tascioglu <onur.tascioglu@ardictech.com>
2024-04-06 15:57:16 +0300

    updated test

--- test2.py
+++ test2.py
@@ -11,15 +11,18 @@ steps = 2000
 print("Single coil steps")
 print("Steps: ", steps)
 direction_pin.on()
-for _ in range(steps):
+for i in range(steps):
+    print("Moving direction 0," , i)
     step_pin.on()
-    time.sleep(0.001)
+    time.sleep(0.01)
     step_pin.off()
-    time.sleep(0.001)
+    time.sleep(0.01)
 
+print("Steps: ", steps)
 direction_pin.off()
-for _ in range(steps):
+for i  in range(steps):
+    print("Moving direction 1," , i)
     step_pin.on()
-    time.sleep(0.001)
+    time.sleep(0.01)
     step_pin.off()
-    time.sleep(0.001)
+    time.sleep(0.01)
