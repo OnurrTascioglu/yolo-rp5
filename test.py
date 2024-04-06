2872b1e7dc47dfbb31c3dec568a419ca5c60770b
onur.tascioglu <onur.tascioglu@ardictech.com>
2024-04-06 12:42:28 +0300

    updated test

--- test.py
+++ test.py
@@ -4,17 +4,18 @@ import atexit
 from constants import *
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_StepperMotor
 from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
 
 # create a default object, no changes to I2C address or frequency
-mh = Adafruit_MotorHAT(addr=0x70,freq=60)
+# mh = Adafruit_MotorHAT(addr=0x70,freq=60)
 
-def turnOffMotors():
-    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+# def turnOffMotors():
+#     mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+#     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+#     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+#     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
-atexit.register(turnOffMotors)
+# atexit.register(turnOffMotors)
 
 # myStepper = mh.getStepper(200, 1)
 # myStepper.setSpeed(30)
@@ -37,32 +38,32 @@ atexit.register(turnOffMotors)
 
 # atexit.register(turnOffMotors)
 
+kit = MotorKit()
 
 
+if sys.argv[1] == "--left":
+    for i in range(1000):
+        print(i)
+        kit.stepper1.onestep()
+        time.sleep(0.005)
 
-# if sys.argv[1] == "--left":
-#     for i in range(2000):
-#         print(i)
-#         kit.stepper1.onestep(direction=stepper.FORWARD, style=Mo.MICROSTEP)
-#         time.sleep(0.005)
+if sys.argv[1] == "--right":
+    for i in range(1000):
+        print(i)
+        kit.stepper1.onestep(direction=2)
+        time.sleep(0.005)
 
-# if sys.argv[1] == "--right":
-#     for i in range(2000):
-#         print(i)
-#         kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
-#         time.sleep(0.005)
+if sys.argv[1] == "--up":
+    for i in range(1000):
+        print(i)
+        kit.stepper2.onestep()
+        time.sleep(0.005)
 
-# if sys.argv[1] == "--up":
-#     for i in range(2000):
-#         print(i)
-#         kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
-#         time.sleep(0.005)
+if sys.argv[1] == "--down":
+    for i in range(1000):
+        print(i)
+        kit.stepper2.onestep(direction=2)
+        time.sleep(0.005)
 
-# if sys.argv[1] == "--down":
-#     for i in range(2000):
-#         print(i)
-#         kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
-#         time.sleep(0.005)
-
-# kit.stepper1.release()
-# kit.stepper2.release()
+kit.stepper1.release()
+kit.stepper2.release()
